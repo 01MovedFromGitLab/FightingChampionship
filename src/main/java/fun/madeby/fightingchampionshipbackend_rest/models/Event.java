7c41327982de9264f839fd576fb4560641b97009
Graham Duthie <grahamd483@gmail.com>
2021-07-12 18:30:14 +0200

    Venue and Event entities added, not created/tested yet

--- src/main/java/fun/madeby/fightingchampionshipbackend_rest/models/Event.java
+++ src/main/java/fun/madeby/fightingchampionshipbackend_rest/models/Event.java
@@ -1,4 +1,31 @@
 package fun.madeby.fightingchampionshipbackend_rest.models;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
 public class Event {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank
+	@Column(name="Name", columnDefinition = "VARCHAR(250) NOT NULL")
+	private String name;
+	@NotBlank
+	@Column(name="Date", columnDefinition = "")
+	@DateTimeFormat(pattern = "yyyy-MM-dd-HH:mm")
+	private LocalDateTime startDateTime;
+	@ManyToOne
+	@JoinColumn(name="VenueId")
+	private Venue venue;
+
+
+
 }
--- src/main/java/fun/madeby/fightingchampionshipbackend_rest/models/Venue.java
+++ src/main/java/fun/madeby/fightingchampionshipbackend_rest/models/Venue.java
@@ -1,4 +1,27 @@
 package fun.madeby.fightingchampionshipbackend_rest.models;
 
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
 public class Venue {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank
+	@Column(name="Name", columnDefinition = "VARCHAR(150) NOT NULL")
+	private String name;
+	@NotBlank
+	@Column(name="Name", columnDefinition = "VARCHAR(500) NOT NULL")
+	private String address;
+	@OneToMany(mappedBy = "venue", cascade = {CascadeType.PERSIST,
+		CascadeType.MERGE, CascadeType.DETACH})
+	private Set<Event> events = new HashSet<>();
 }
